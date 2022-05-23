0add25eb69071cc1732f28d5bcf02dc428d3b263
Abir-Hendawy <90090782+Abir-Hendawy@users.noreply.github.com>
2022-05-23 18:34:21 +0200

    upload assignment 6

---
+++ queries.sql
@@ -0,0 +1,7 @@
+SELECT  DISTINCT orderNumber,count(productCode) ,priceEach FROM `orderdetails` WHERE priceEach>100 and productCode like'%s18%'
+group by orderNumber;
+
+SELECT * FROM `payments` WHERE paymentDate like '%-%-05' or paymentDate like '%-%-06';
+
+SELECT customerNumber,creditLimit,country,phone FROM `customers` where  country='USA' and phone like '%5555%'
+order by creditLimit desc limit 5;
